3b34535f2cd2c29d222778244450101fd0dc8a5a
tybjet <161176253+tybjet@users.noreply.github.com>
2024-04-27 04:01:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-// Example JavaScript code
-alert("Hello, World!");
+
+function displayWelcomeMessage() {
+    alert("Welcome to our website!");
+}
+
+
+window.onscroll = function() {
+    // Call the displayWelcomeMessage function when the user scrolls
+    displayWelcomeMessage();
+};
